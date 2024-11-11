67712f559c295819c2649c11cf5a31bcfdade519
Jinsu <injnamek@gmail.com>
2024-11-11 15:12:59 +0900

    컴포넌트 모듈화 기능 추가

--- workspace-Jinsu/ch01-buildup/react/05/src/pages/Todo.jsx
+++ workspace-Jinsu/ch01-buildup/react/05/src/pages/Todo.jsx
@@ -3,13 +3,17 @@ import TodoList from './TodoList';
 
 function Todo(props) {
   return (
-    <div id="main">
-      <div id="container">
+    <div id='main'>
+      <div id='container'>
         <ul>
           <li>
             <h2>쇼핑 목록</h2>
             <TodoInput addItem={props.addItem} />
-            <TodoList itemList={props.itemList} />
+            <TodoList
+              itemList={props.itemList}
+              toggleDone={props.toggleDone}
+              deleteItem={props.deleteItem}
+            />
           </li>
         </ul>
       </div>
