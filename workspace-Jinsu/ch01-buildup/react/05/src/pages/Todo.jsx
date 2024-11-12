900822c456a3468ece2935bb86e810f5fc250adb
Jinsu <injnamek@gmail.com>
2024-11-12 15:47:32 +0900

    counter 예제 vite 환경의 React로 만들어보기

--- workspace-Jinsu/ch01-buildup/react/05/src/pages/Todo.jsx
+++ workspace-Jinsu/ch01-buildup/react/05/src/pages/Todo.jsx
@@ -14,6 +14,10 @@ function Todo(props) {
               toggleDone={props.toggleDone}
               deleteItem={props.deleteItem}
             />
+            {/* <TodoList
+              // 귀찮아서 props를 다 전달하기는 했지만 만약 TodoList에서 addItem 함수를 절대로 쓰면 안 된다면? 예상치 못한 에러가 날 수 있어서 그냥 안 넘기는 게 안전하다.
+              {...props}
+            /> */}
           </li>
         </ul>
       </div>
