25702ac2759cdf3f18cc6bd1cf358ac21613e235
Jinsu <injnamek@gmail.com>
2024-11-12 15:05:57 +0900

    cra, vite로 react project 환경설정

---
+++ workspace-Jinsu/ch02-start/vite/src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css';
+import Todo from './pages/todo/Todo';
+
+function App() {
+  return <Todo />;
+}
+
+export default App;
---
+++ workspace-Jinsu/ch02-start/vite/src/pages/todo/Todo.jsx
@@ -0,0 +1,12 @@
+import Title from '@components/Title';
+
+const Todo = () => {
+  return (
+    <div>
+      <Title />
+      <p>Todo 목록</p>
+    </div>
+  );
+};
+
+export default Todo;
