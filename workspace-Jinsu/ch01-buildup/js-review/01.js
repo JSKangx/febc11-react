73eab56210a7c49351bc4ad2f3646158d9504f65
Jinsu <injnamek@gmail.com>
2024-11-13 11:19:40 +0900

    js 다시 배우기

--- workspace-Jinsu/ch01-buildup/js-review/01.js
+++ workspace-Jinsu/ch01-buildup/js-review/01.js
@@ -1,7 +1,38 @@
 var arr = [10, 20, 30];
 
-// 각 요소의 제곱값으로 구성된 새로운 배열 생성
-var arr2 = [100, 400, 900];
+// 하드 코딩 : 각 요소의 제곱값으로 구성된 새로운 배열 생성
+// var arr2 = [100, 400, 900];
 
+// 여러가지 방식으로 arr2를 만들어보자
+// (1) for
+var arr2 = [];
+for (let i = 0; i < arr.length; i++) {
+  arr2.push(arr[i] * arr[i]);
+}
 
-console.log(arr2);  // [100, 400, 900]
+// (2) for of
+var arr2 = [];
+for (const item of arr) {
+  arr2.push(item * item);
+}
+
+// (3) forEach()
+var arr2 = [];
+arr.forEach(function (item) {
+  arr2.push(item * item);
+});
+
+// (4) map
+var arr2 = [];
+arr2 = arr.map(function (item) {
+  return item * item;
+});
+
+// (5) forEach, map의 콜백함수를 화살표 함수로
+var arr2 = [];
+arr.forEach(item => arr2.push(item * item));
+
+var arr2 = [];
+arr2 = arr.map(item => item * item);
+
+console.log(arr2); // [100, 400, 900]
--- workspace-Jinsu/ch01-buildup/js-review/02.js
+++ workspace-Jinsu/ch01-buildup/js-review/02.js
@@ -1,9 +1,26 @@
 var itemList = [
-  { no: 1, todo: '두부', done: true},
-  { no: 2, todo: '계란', done: false},
-  { no: 3, todo: '라면', done: true},
+  { no: 1, todo: '두부', done: true },
+  { no: 2, todo: '계란', done: false },
+  { no: 3, todo: '라면', done: true },
 ];
 
 console.log(itemList[0]);
 console.log(itemList[1]);
 
+// (1) 배열 구조분해 할당
+var [first, second, third] = itemList;
+console.log(first);
+console.log(second);
+console.log(third);
+// useState에서 많이 썼다.
+// const [count, setCount] = useState();
+
+// (2) 객체 구조분해 할당
+console.log(second.no, second.todo, second.done);
+// 위 문장을 객체 구조분해 할당으로 표현해보자
+var { no, todo, done } = second;
+console.log(no, todo, done); // '객체.속성' 으로 접근하지 않고도 사용 가능
+// props 할 때 이걸 많이 썼다.
+function myFun(second) {}
+// 객체 구조 분해 할당으로 second 객체의 속성을 분해해서 받아올 수 있다.
+function myFun2({ no, todo, done }) {}
