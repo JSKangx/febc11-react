af1154d3e94d1044ccc521de63fed05adb5eef92
Jinsu <injnamek@gmail.com>
2024-11-14 14:41:58 +0900

    state를 한 번에 여러번 업데이트할 때의 문제점

--- workspace-Jinsu/ch02-start/state/02/src/App.jsx
+++ workspace-Jinsu/ch02-start/state/02/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [number, setNumber] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>02 이벤트 핸들러에서 state 값을 여러번 변경했을 때 문제점</h2>
+      <p>{number}</p>
+      <button
+        onClick={() => {
+          console.log('클릭 처리 시작', number);
+          setNumber(num => num + 1);
+          setNumber(num => num + 1);
+          console.log('클릭 처리 종료', number);
+        }}
+      >
+        +2
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
