49c7092716a6d05cecf714534c69999b755d07d4
Jinsu <injnamek@gmail.com>
2024-11-23 16:37:46 +0900

    feature: TodoList 라우터 구현하기

--- workspace-Jinsu/app/todolist/04-router/src/components/Header.jsx
+++ workspace-Jinsu/app/todolist/04-router/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Header() {
   return (
@@ -8,13 +8,19 @@ function Header() {
         <div>
           <ul>
             <li>
-              <Link to='home'>Home</Link>
+              <NavLink className={({ isActive }) => (isActive ? 'menu-dark' : 'menu')} to='home'>
+                Home
+              </NavLink>
             </li>
             <li>
-              <Link to='about'>About</Link>
+              <NavLink className={({ isActive }) => (isActive ? 'menu-dark' : 'menu')} to='about'>
+                About
+              </NavLink>
             </li>
             <li>
-              <Link to='list'>TodoList</Link>
+              <NavLink className={({ isActive }) => (isActive ? 'menu-dark' : 'menu')} to='list'>
+                TodoList
+              </NavLink>
             </li>
           </ul>
         </div>
--- workspace-Jinsu/app/todolist/04-router/src/routes.jsx
+++ workspace-Jinsu/app/todolist/04-router/src/routes.jsx
@@ -1,7 +1,12 @@
 import Layout from '@components/Layout';
 import About from '@pages/About';
+import ErrorPage from '@pages/ErrorPage';
 import Home from '@pages/Home';
-import { createBrowserRouter } from 'react-router-dom';
+import TodoAdd from '@pages/TodoAdd';
+import TodoDetail from '@pages/TodoDetail';
+import TodoEdit from '@pages/TodoEdit';
+import TodoList from '@pages/TodoList';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 
 const router = createBrowserRouter(
   [
@@ -9,8 +14,14 @@ const router = createBrowserRouter(
       path: '/',
       element: <Layout />,
       children: [
+        { index: true, element: <Navigate to='/home' /> },
         { path: 'home', element: <Home /> },
         { path: 'about', element: <About /> },
+        { path: 'list', element: <TodoList /> },
+        { path: 'todoadd', element: <TodoAdd /> },
+        { path: 'tododetail', element: <TodoDetail /> },
+        { path: 'todoedit', element: <TodoEdit /> },
+        { path: '*', element: <ErrorPage /> },
       ],
     },
   ],
