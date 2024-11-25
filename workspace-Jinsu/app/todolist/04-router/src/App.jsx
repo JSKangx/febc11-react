838f19b93c78421c08da6e9b2115aab7398fb3dd
Jinsu <injnamek@gmail.com>
2024-11-25 13:45:37 +0900

    feature: 레이지 로드 적용

--- workspace-Jinsu/app/todolist/04-router/src/App.jsx
+++ workspace-Jinsu/app/todolist/04-router/src/App.jsx
@@ -1,10 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
-import router from './routes';
+// import router from './routes';
+import router from './routes-lazy';
+import { Suspense } from 'react';
+import { BarLoader } from 'react-spinners';
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} future={{ v7_startTransition: true }} />
+      <Suspense fallback={<BarLoader color='#94d0ef' />}>
+        <RouterProvider router={router} future={{ v7_startTransition: true }} />
+      </Suspense>
     </>
   );
 }
