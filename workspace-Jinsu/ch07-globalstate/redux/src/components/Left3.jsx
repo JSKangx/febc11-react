874a75dc3c7daf55ded65b13888ba85a8b0083b7
Jinsu <injnamek@gmail.com>
2024-11-28 13:41:19 +0900

    style: redux 주석 추가

--- workspace-Jinsu/ch07-globalstate/redux/src/components/Left3.jsx
+++ workspace-Jinsu/ch07-globalstate/redux/src/components/Left3.jsx
@@ -6,10 +6,11 @@ function Left3() {
     console.log('      # Left3 렌더링.');
   });
 
-  // redux 이용한 상태관리
-  // useSelector에는 함수를 하나 전달함.
+  // useSelector는 Redux의 상태에서 특정 값을 선택해 가져오는 훅이다.
+  // useSelector에는 함수를 하나 전달함. 이 함수에 전달되는 매개변수는 Redux 스토어의 전체 상태 객체를 의미한다.
   // 상태를 전달받고, 상태에서 원하는 값을 리턴해주는 것
   // 함수에 전달되는 인자값은 counterReducer가 리턴해주는 값.
+  // useSelector로 count 상태를 구독하는 컴포넌트만 리렌더링된다.
   const count = useSelector((state) => state.count);
 
   return (
--- workspace-Jinsu/ch07-globalstate/redux/src/components/Right3.jsx
+++ workspace-Jinsu/ch07-globalstate/redux/src/components/Right3.jsx
@@ -1,13 +1,22 @@
+import counterActionCreator from '@redux/counterActionCreator';
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 function Right3() {
-  useEffect(()=>{
+  useEffect(() => {
     console.log('      # Right3 렌더링.');
   });
+
+  // 상태값 수정하기 위해 useDispatch 훅으로 dispatch 함수를 불러옴.
+  // 이 함수를 사용하기 위해서 인자값으로 사용할 객체의 메서드를 넣어줘야 함.
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h3>Right3</h3>
-      <button onClick={ () => {} }>+1</button>
+      <button onClick={() => dispatch(counterActionCreator.countDown(1))}>-1</button>
+      <button onClick={() => dispatch(counterActionCreator.countReset())}>0</button>
+      <button onClick={() => dispatch(counterActionCreator.countUp(1))}>+1</button>
     </div>
   );
 }
