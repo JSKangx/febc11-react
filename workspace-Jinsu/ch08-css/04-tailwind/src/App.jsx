0dc7305250041f1b03ea91fcd1c907fd27146e5f
Jinsu <injnamek@gmail.com>
2024-12-03 11:13:46 +0900

    tailwind로 리액트 컴포넌트 스타일링하기

--- workspace-Jinsu/ch08-css/04-tailwind/src/App.jsx
+++ workspace-Jinsu/ch08-css/04-tailwind/src/App.jsx
@@ -1,22 +1,24 @@
 import Button from '@components/Button';
-import './App.css';
 import Login from './Login';
 
 function App() {
   return (
     <>
       <h1>04 Tailwind CSS 사용</h1>
-      <div className='container'>
-        <Button>그냥 버튼</Button>
-        <Button bg='blue' color='red'>
+      <div className='w-60 mt-3 mx-auto'>
+        <Button size='sm'>그냥 버튼</Button>
+        <Button bg='blue' color='red' size='lg'>
           파란 배경의 빨간 글자
         </Button>
-        <Button bg='yellow' color='red'>
+        <Button bg='yellow' color='red' size='md'>
           노란 배경의 빨간 글자
         </Button>
-        <Button bg='gray' color='blue'>
+        <Button bg='gray' color='blue' size='sm'>
           회색 배경의 파란 글자
         </Button>
+        <button className='btn '>일반 버튼</button>
+        <button className='btn btn-primary'>등록</button>
+        <button className='btn btn-warn'>삭제</button>
       </div>
       <Login />
     </>
--- workspace-Jinsu/ch08-css/04-tailwind/src/components/Button.jsx
+++ workspace-Jinsu/ch08-css/04-tailwind/src/components/Button.jsx
@@ -1,9 +1,32 @@
 import PropTypes from 'prop-types';
-import './Button.css';
 
-const Button = ({ type = 'button', onClick, children, color, bg }) => {
+// onClick, type은 받아서 무언갈 하는 게 아니기 때문에 그냥 ...rest로 받고, 그대로 사용.
+const Button = ({ children, color = 'black', bg = 'gray', size = 'md', ...rest }) => {
+  let bgColor = {
+    gray: 'bg-slate-300',
+    blue: 'bg-blue-300',
+    red: 'bg-red-300',
+    yellow: 'bg-yellow-300',
+  };
+
+  let textColor = {
+    black: 'text-black',
+    white: 'text-white',
+    blue: 'text-blue-600',
+    red: 'text-red-600',
+  };
+
+  let btnSize = {
+    sm: 'py-1 px-2 text-sm',
+    md: 'py-2 px-4 text-base',
+    lg: 'py-2 px-6 text-lg',
+  };
+
   return (
-    <button type={type} onClick={onClick} className={`button color-${bg}-${color}`}>
+    <button
+      className={`${bgColor[bg]} ${textColor[color]} ${btnSize[size]} m-1 rounded-md shadow-md `}
+      {...rest}
+    >
       {children}
     </button>
   );
@@ -12,8 +35,9 @@ const Button = ({ type = 'button', onClick, children, color, bg }) => {
 Button.propTypes = {
   children: PropTypes.string.isRequired,
   type: PropTypes.string,
-  color: PropTypes.oneOf(['blue', 'red', 'yellow', 'white']), // (글자 색상) 배열값중 하나만 받아야 한다.
+  color: PropTypes.oneOf(['blue', 'red', 'black', 'white']), // (글자 색상) 배열값중 하나만 받아야 한다.
   bg: PropTypes.oneOf(['blue', 'red', 'yellow', 'gray']), // 배경 색상
+  size: PropTypes.oneOf(['sm', 'md', 'lg']),
   onClick: PropTypes.func,
 };
 
