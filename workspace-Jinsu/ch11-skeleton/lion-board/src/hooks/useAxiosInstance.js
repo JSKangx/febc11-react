cf8f1c0ce755731629203af8db0eadeaae2b8da0
Jinsu <injnamek@gmail.com>
2024-12-12 10:46:08 +0900

    .

--- workspace-Jinsu/ch11-skeleton/lion-board/src/hooks/useAxiosInstance.js
+++ workspace-Jinsu/ch11-skeleton/lion-board/src/hooks/useAxiosInstance.js
@@ -14,7 +14,7 @@ function useAxiosInstance() {
   // 기본 설정을 가진 새로운 인스턴스를 생성할 때 사용하는 메서드.
   // 매 요청마다 공통된 설정을 적용할 수 있다.
   const instance = axios.create({
-    baseURL: 'https:/11.fesp.shop',
+    baseURL: 'https://11.fesp.shop',
     timeout: 1000 * 15,
     headers: {
       'Content-Type': 'application/json',
